bd5f9d6d4a1ba1dbf539bf4b3a874063c7921b91
Erik <erik.nilsson3@yh.nackademin.se>
2026-03-31 15:20:35 +0200

    Replaced the placeholder for the href in index.html and updated the main.js file with safer excecution. (Preventing crashes if element isnt found)

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-document.getElementById("order-form").addEventListener("submit", function(e) {
-    e.preventDefault();
+//Updaterade koden för att göra det enklare och säkrare när man ska placera en order
+//genom att lägga logiken i en if statement.
 
-    const modal = new bootstrap.Modal(document.getElementById("orderModal"));
-    modal.show();
+document.addEventListener("DOMContentLoaded", () =>  {
+    const form = document.getElementById("order-form");
+
+    if (form){
+        form.addEventListener("submit", function(e){
+            e.preventDefault();
+            const modal = new bootstrap.Modal(document.getElementById("orderModal"));
+            modal.show();
+        });
+    }
 });
-
